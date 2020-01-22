89ab13408b8296ca9e4003035a3431b7a001e3a2
tamotsu ichiba <ichibat@gmail.com>
2020-01-22 10:06:38 +0900

    improved

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import UserRegister from '../components/UserRegister.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/user_register',
+    name: 'UserRegister',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "UserRegister" */ '../components/UserRegister.vue')
   }
 ]
 
